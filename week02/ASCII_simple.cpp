380781ae587f0f2f009177472253b2253bb2c169
Maryam Majedi <majedi@ucsb.edu>
2025-10-08 10:34:22 -0700

    week02

---
+++ week02/ASCII_simple.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Declare a variable 'c' of type char and assign it the character 'A'
+    char c = 'A';
+    cout << "Character: " << c << endl;
+
+    // Print the ASCII (integer) code of 'c' by converting it to an int type
+    cout << "ASCII code: " << int(c) << endl;
+
+    return 0;
+}
