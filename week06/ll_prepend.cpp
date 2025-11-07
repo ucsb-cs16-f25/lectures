09e8a25a7b9b1b3269f8415b8149f73dbe741880
Maryam Majedi <majedi@ucsb.edu>
2025-11-06 18:28:20 -0800

    week06

---
+++ week06/ll_prepend.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+Node *createNode(int value)
+{
+    Node *newNode = new Node;
+    newNode->data = value;
+    newNode->next = nullptr;
+    return newNode;
+}
+
+// void prepend(Node *head, int value) // head is passed by value, meaning that any changes made inside prepend() do not reflect in main()
+// {
+//     Node *newNode = createNode(value);
+//     newNode->next = head; // ❌ This only modifies the local copy of `head`, not the actual head in `main()`
+//     head = newNode;
+// }
+
+void prepend(Node *&head, int value) // ✅ Pass by reference
+{
+    Node *newNode = createNode(value);
+    newNode->next = head;
+    head = newNode; // ✅ Now head in main() is updated
+}
+
+void printList(Node *head)
+{
+    Node *curr = head;
+    while (curr)
+    {
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
+    cout << endl;
+}
+
+void destroyList(Node *&head)
+{
+    while (head)
+    {
+        Node *next = head->next;
+        delete head;
+        head = next;
+    }
+}
+
+int main()
+{
+    // Node *head; // ❌ Points to garbage value
+    Node *head = nullptr;
+    prepend(head, 4);
+    prepend(head, 8);
+    prepend(head, 2);
+    printList(head);
+
+    destroyList(head);
+    printList(head);
+
+    return 0;
+}
